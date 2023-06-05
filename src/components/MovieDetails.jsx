85e424d85167f824ab84f53659b36abf573cc286
apav2086 <apav2086@gmail.com>
2023-06-05 19:49:09 -0400

    work in progress -2

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,36 +1,36 @@
-import { useEffect, useState } from "react";
-import { Outlet, useParams } from "react-router-dom";
-import { Link } from 'react-router-dom';
+import { useState } from "react";
+import { Outlet} from "react-router-dom";
+import { Link} from 'react-router-dom';
 
 export default function MovieDetails() {
-  const { movieID } = useParams();
-  const [movie, setMovie] = useState('');
-  const [from, setFrom] = useState(null);
+ 
   const [data, setData] = useState(() => moviesData.filter(movie =>
     movie))
  
   return (
-      <div>
-          <Link to="/">Back</Link>
+    <div>
+      <Link to="/">Back</Link>
       {data && (
         <div>
           <h3>{data.title}</h3>
           <img src={data.poster_path} alt="movie poster" />
-                  <h4>User Score: {data.score }</h4>
+          <h4>User Score: {data.score}</h4>
           <h5>Overview: {data.overview}</h5>
           <h5>Genres: {data.genres}</h5>
-                  <h5>Additional information</h5>
-                  <ul>
-                       <li>
-          <Link to={`/movies/${data.Id}/cast`} >Cast</Link>
-        </li>
-        <li>
-          <Link to={`/movies/${data.Id}/reviews`} >Reviews</Link>
-        </li>
+          <h5>Additional information</h5>
+          <ul>
+            <li>
+              <Link to={`/movies/${data.Id}/cast`} >Cast</Link>
+            </li>
+            <li>
+              <Link to={`/movies/${data.Id}/reviews`} >Reviews</Link>
+            </li>
           </ul>
-            <Outlet />
+          <Outlet />
         </div>
-         </div>
-      );
-     
+       
+      )
+      }
+    </div>
+  );
 }
