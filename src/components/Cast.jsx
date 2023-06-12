8fedc6f49076e6edc5a3116807958937372ddb9f
apav2086 <apav2086@gmail.com>
2023-06-12 18:36:54 -0400

    cast correction

--- src/components/Cast.jsx
+++ src/components/Cast.jsx
@@ -1,28 +1,36 @@
 import { useState, useEffect } from 'react';
-import { useParams } from "react-router-dom";
+import { useParams } from 'react-router-dom';
 import { getCast } from '../services/movies-api';
 import css from '../styles/cast.module.css';
 
 export default function Cast() {
-    const { movieId } = useParams();
-    const [cast, setCast] = useState([]);
+  const { movieId } = useParams();
+  const [cast, setCast] = useState([]);
 
-    useEffect(() => {
-        getCast(movieId).then(data => {
-            setCast(data.cast);
-        })
-    }, [movieId]);
+  useEffect(() => {
+    getCast(movieId).then(data => {
+      setCast(data.cast);
+    });
+  }, [movieId]);
 
-    return (
-        <div>
-            {cast && (
-                <ul className={css.list}>{cast.map(({ id, character, original_name, profile_path }) => (
-                < li className={css.item} key={id} >
-                        <img
-                            src={`https://image.tmdb.org/t/p/w500${profile_path}`} width={'120 px'} alt={original_name} />
-                    <p>{original_name}</p>
-                    <p>{character}</p>
-                </li>))}
-            </ul>)}
-        </div>);
+  console.log(cast);
+
+  return (
+    <div>
+      {cast && (
+        <ul className={css.list}>
+          {cast.map(({ id, character, original_name, profile_path }) => (
+            <li className={css.list} key={id}>
+              <img
+                src={`https://image.tmdb.org/t/p/w500${profile_path}`} width={'120 px'}
+                alt={original_name}
+              />
+              <p>{original_name}</p>
+              <p>{character}</p>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -14,15 +14,16 @@ export default function Reviews() {
 
     return (
         <div>
+            {reviews.length > 0 ? (
             <ul>
-                {reviews.length > 0 ? (
-                        reviews.map((review) => (
-                            < li key={review.id} >
-                                <h4>{review.author}</h4>
-                                <p>{review.content}</p>
-                            </li>))
+                    {reviews.map((review => (
+                        < li key={review.id} >
+                            <h4>{review.author}</h4>
+                            <p>{review.content}</p>
+                        </li>)))}
+                     </ul>
                         ) : (<h4>We don't have any reviews for this movie.</h4>)}
-                </ul>
+               
             
     </div>
     );
